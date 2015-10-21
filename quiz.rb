b2c60fb9344168f077902f69271d6b0ebe7d5253
Eunice <omgunis@gmail.com>
2015-10-20 18:43:30 -0700

    Eunice submitting quiz

--- quiz.rb
+++ quiz.rb
@@ -1,11 +1,33 @@
-def divisor
-
+puts "quiz loaded"
+def divisor(n)
+  output = Array.new
+  for i in 1..n do
+    if n % i == 0
+      output << i
+    end
+  end
+  return output
 end
 
-def prime
-
+def prime(n)
+  output = Array.new
+  for i in 1..n do
+    if divisor(i).length == 2
+      output << i
+    end
+  end
+  return output
 end
 
-def fib
-
+def fib(n)
+  output = [1,1]
+  for i in 2..(n-1) do
+    sum = output[i-1] + output[i-2] #prints fib sequence
+    if sum < n #stops pushing numbers when index is greater than n
+      output << sum
+    else
+      break
+    end
+  end
+  return output
 end
